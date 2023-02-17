852f9d5c3a8e59998fa3406043f9775303268044
Rayane Gaudêncio <rayanegaudencio11@gmail.com>
2023-02-17 13:23:23 -0300

    adicionado comentário no try/catch with resources

--- java_pilha/src/java_pilha/TesteConexao.java
+++ java_pilha/src/java_pilha/TesteConexao.java
@@ -3,11 +3,11 @@ package java_pilha;
 public class TesteConexao {
 	public static void main(String[] args) {
 
-		// ctry-with-resources
+		// try-with-resources, a classe conexão extende a classe AutoCoseable, não sendo necessário o fechamento de todas conexões
 		try(Conexao conexao = new Conexao()) {
 			conexao.leDados();
 		} catch(Exception ex) {
-			System.out.println("Deu erro");
+			System.out.println(ex.getMessage());
 		}
 		
 		
